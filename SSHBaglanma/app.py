8a31906a9effbd08e7751422a224c3d778d730ae
tarikkilic <tarikkilic@saatteknoloji.com>
2022-05-24 11:57:17 +0300

    24.05.2022

--- SSHBaglanma/app.py
+++ SSHBaglanma/app.py
@@ -1,17 +1,35 @@
-from pexpect import pxssh
+# from pexpect import pxssh
+#
+# ip= "10.98.228.146"
+# myusername= "root"
+# mypassword="Cms2023@Netas.iptv"
+#
+#
+# s = pxssh.pxssh()
+# if not s.login ('ip', '22','myusername', 'mypassword'):
+#     print ("SSH session failed on login.")
+#     print (str(s))
+# else:
+#     print ("SSH session login successful")
+#     s.sendline ('ls -l')
+#     s.prompt()         # match the prompt
+#     print (s.before)     # print everything before the prompt.
+#     s.logout()
 
-ip= "10.98.228.146"
-myusername= "root"
-mypassword="Cms2023@Netas.iptv"
 
+import paramiko
 
-s = pxssh.pxssh()
-if not s.login ('ip', '22','myusername', 'mypassword'):
-    print ("SSH session failed on login.")
-    print (str(s))
-else:
-    print ("SSH session login successful")
-    s.sendline ('ls -l')
-    s.prompt()         # match the prompt
-    print (s.before)     # print everything before the prompt.
-    s.logout()
+host = "10.98.228.146"
+port = 22
+username = "root"
+password = "Cms2023@Netas.iptv"
+
+command = "ls -ltr"
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(host, port, username, password)
+
+stdin, stdout, stderr = ssh.exec_command(command)
+lines = stdout.readlines()
+print(lines)
