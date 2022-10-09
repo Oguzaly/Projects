9c52c2853825b4b6bc20c0fb35c0f5a639de32b9
Oguzaly <89504819+Oguzaly@users.noreply.github.com>
2022-10-10 00:28:05 +0300

    Update app.py

--- WaitingForAuditing/app.py
+++ WaitingForAuditing/app.py
@@ -1,22 +1,13 @@
 import requests
 from openpyxl import Workbook,load_workbook
-
-
 wp = load_workbook('C:/Users/HP/Desktop/playground/WaitingForAuditing/guid.xlsx')
 ws = wp.active
-
 with open('C:/Users/HP/Desktop/playground/WaitingForAuditing/status.csv','a') as f:
-
     for row in ws['A']:
-
-
         url = "http://10.98.225.184:8084/mw/api/getcontentinfo?id="+'{}'.format(row.value)+"&type=1"
-
         payload={}
         headers = {}
-
         response = requests.request("GET", url, headers=headers, data=payload)
-
         print(response.text)
         print(i)
         f.write(response.text+'\n')
